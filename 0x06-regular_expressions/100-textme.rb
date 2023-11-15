9b8fdb899d52d502baf1fcff7cde4e52a5c9be9c
nashi-san <hamouda.ihsan@gmail.com>
2023-11-15 00:18:08 +0000

    advanced

--- 0x06-regular_expressions/100-textme.rb
+++ 0x06-regular_expressions/100-textme.rb
@@ -1,2 +1,6 @@
 #!/usr/bin/env ruby
-puts "#{sender},#{number},#{flags}".scan(/^.*\[from:(.*?)\].*\[to:(.*?)\].*\[flags:(.*?)\].*$/).join
+if ARGV.length == 1
+  sender, number, flags = ARGV[0].match(/^.*\[from:(.*?)\].*\[to:(.*?)\].*\[flags:(.*?)\].*$/i).captures
+  puts "#{sender},#{number},#{flags}"
+  exit
+end
