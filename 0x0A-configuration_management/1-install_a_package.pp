3c379765e911459a1014febc6cf171332d742bf4
nashi-san <hamouda.ihsan@gmail.com>
2023-12-22 23:32:05 +0000

    puppet

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,21 @@
 #!/usr/bin/pup
-#installing flask
+# Install Python 3.8.10
+package { 'python3.8':
+  ensure => '3.8.10',
+}
+# Install pip
+package { 'python3-pip':
+  ensure => present,
+}
+# Install Flask 2.1.0
 package { 'flask':
   ensure   => '2.1.0',
-  provider => 'pip3'
+  provider => 'pip',
+  require  => Package['python3-pip'],
+}
+# Install Werkzeug
+package { 'werkzeug':
+  ensure   => '2.1.1',
+  provider => 'pip',
+  require  => Package['python3-pip'],
 }
